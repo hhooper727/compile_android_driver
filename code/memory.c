c99cbfb33a4a06b12bfe3dc7e7d64dd4e8e45a92
hhooper727 <905108379@qq.com>
2025-11-04 21:48:07 +0800

    Update memory.c

--- code/memory.c
+++ code/memory.c
@@ -110,7 +110,7 @@ phys_addr_t translate_linear_address(struct mm_struct *mm, uintptr_t va)
 #endif
 
 #ifndef ARCH_HAS_VALID_PHYS_ADDR_RANGE
-static inline int valid_phys_addr_range(phys_addr_t addr, size_t count)
+static inline int my_valid_phys_addr_range(phys_addr_t addr, size_t count)
 {
 	return addr + count <= __pa(high_memory);
 }
@@ -124,7 +124,7 @@ bool read_physical_address(phys_addr_t pa, void *buffer, size_t size)
 	{
 		return false;
 	}
-	if (!valid_phys_addr_range(pa, size))
+	if (!my_valid_phys_addr_range(pa, size))
 	{
 		return false;
 	}
@@ -150,7 +150,7 @@ bool write_physical_address(phys_addr_t pa, void *buffer, size_t size)
 	{
 		return false;
 	}
-	if (!valid_phys_addr_range(pa, size))
+	if (!my_valid_phys_addr_range(pa, size))
 	{
 		return false;
 	}
