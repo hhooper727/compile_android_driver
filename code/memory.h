64a6725393334b71878a9233c958d2292a5d7cd5
hhooper727 <905108379@qq.com>
2025-11-04 16:17:52 +0800

    Update memory.h

--- code/memory.h
+++ code/memory.h
@@ -10,3 +10,6 @@ bool write_physical_address(phys_addr_t pa, void *buffer, size_t size);
 bool read_process_memory(pid_t pid, uintptr_t addr, void *buffer, size_t size);
 
 bool write_process_memory(pid_t pid, uintptr_t addr, void *buffer, size_t size);
+#ifndef ARCH_HAS_VALID_PHYS_ADDR_RANGE
+int valid_phys_addr_range(phys_addr_t addr, size_t count);
+#endif
